82d15be30ed23d98c53d0552decdd5280f532521
Rodrigo Rodrigues Leal da Silva <rr.leal4@gmail.com>
2022-11-23 18:44:19 -0300

    Refatorando as controllers

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -1,4 +1,6 @@
-﻿using AutoMapper;
+﻿
+using AutoMapper;
+using FilmesApi.Data;
 using FilmesAPI.Data;
 using FilmesAPI.Data.Dtos;
 using FilmesAPI.Models;
@@ -12,51 +14,51 @@ namespace FilmesAPI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class FilmesController : ControllerBase
+    public class FilmeController : ControllerBase
     {
-        private FilmeContext _context;
+        private AppDbContext _context;
         private IMapper _mapper;
 
-        public FilmesController(FilmeContext context, IMapper mapper)
+        public FilmeController(AppDbContext context, IMapper mapper)
         {
             _context = context;
             _mapper = mapper;
         }
-        
+
+
         [HttpPost]
-        public IActionResult AdicionarFilme([FromBody] CreateFilmeDto filmeDto)
+        public IActionResult AdicionaFilme([FromBody] CreateFilmeDto filmeDto)
         {
             Filme filme = _mapper.Map<Filme>(filmeDto);
-           
             _context.Filmes.Add(filme);
             _context.SaveChanges();
-            return CreatedAtAction(nameof(RecuperarFilmesPorId), new {Id=filme.Id}, filme);
-            
+            return CreatedAtAction(nameof(RecuperaFilmesPorId), new { Id = filme.Id }, filme);
         }
 
         [HttpGet]
-        public IEnumerable<Filme> RecuperarFilmes()
+        public IEnumerable<Filme> RecuperaFilmes()
         {
             return _context.Filmes;
         }
 
         [HttpGet("{id}")]
-        public IActionResult RecuperarFilmesPorId(int id)
+        public IActionResult RecuperaFilmesPorId(int id)
         {
             Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
-            if(filme != null)
+            if (filme != null)
             {
                 ReadFilmeDto filmeDto = _mapper.Map<ReadFilmeDto>(filme);
-                return Ok(filme);
+
+                return Ok(filmeDto);
             }
             return NotFound();
         }
 
         [HttpPut("{id}")]
-        public IActionResult AtualizarFilme(int id, [FromBody] UpdateFilmeDto filmeDto)
+        public IActionResult AtualizaFilme(int id, [FromBody] UpdateFilmeDto filmeDto)
         {
             Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
-            if(filme == null)
+            if (filme == null)
             {
                 return NotFound();
             }
@@ -66,7 +68,7 @@ namespace FilmesAPI.Controllers
         }
 
         [HttpDelete("{id}")]
-        public IActionResult DeletarFilme(int id)
+        public IActionResult DeletaFilme(int id)
         {
             Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
             if (filme == null)
--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -1,9 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FilmesApi.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FilmesAPI.Models
 {
@@ -12,14 +10,14 @@ namespace FilmesAPI.Models
         [Key]
         [Required]
         public int Id { get; set; }
-        [Required(ErrorMessage = "O campo título é obrigatório")]
+        [Required(ErrorMessage = "O campo de título é obrigatório")]
         public string Titulo { get; set; }
-        [Required(ErrorMessage = "O campo diretor é obrigatório")]
-        public string Diretor { get; set; }
-        [StringLength(30, ErrorMessage = "O gênero não pode ultrapassar mais de 30 caracteres")]
-        public string Genero { get; set; }
-        [Range(1, 600, ErrorMessage = "A duração deve ter no mínimo 1 e no máximo 600 minutos")]
+        [Required(ErrorMessage = "O campo de duração é obrigatório")]
+        [Range(1, 600, ErrorMessage = "A duração deve ter no mínimo 1 minuto e no máximo 600.")]
         public int Duracao { get; set; }
-        
+        [StringLength(100, ErrorMessage = "O nome do diretor não pode exceder 100 caracteres")]
+        public string Diretor { get; set; }
+        public string Genero { get; set; }
+        public int ClassificacaoEtaria { get; set; }
     }
 }
